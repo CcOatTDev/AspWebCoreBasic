7c473e3dc9a6c5fe69bd8dd45ce72338a12ac421
oattdev <mynameisccoatt@gmail.com>
2020-04-26 23:22:50 +0700

    add source

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using AspWebCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspWebCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+                Title = "The Godfather",
+                ReleaseDate = new DateTime(1972, 3, 24)
+            };
+
+            List<Movie> movies = new List<Movie>();
+            movies.Add(movie);
+
+            return View("index", movies);
+        }
+    }
+}
